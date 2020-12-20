091c044a130b613b4ac9c8d53cefce0cf05b0684
FEZ (Felix Kratzer) <felix@kratzer.org>
2020-12-20 10:40:30 +0100

    Fixed test to match performance improved code

--- tests/test_plingo.py
+++ tests/test_plingo.py
@@ -4,7 +4,7 @@ from plingo import Plingo
 def test_method_names_match_command_id():
     lingo = Plingo()
 
-    for idx, cmd in enumerate(lingo._commands):
+    for idx, cmd in enumerate(lingo._command_names):
         parts = cmd.split("_")
         print(parts)
         assert idx == int(parts[2])
